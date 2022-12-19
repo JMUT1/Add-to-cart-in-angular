85a81709db8c0336e623af770942ed53c5ac24b8
Chema Torres <chemaaa94@gmail.com>
2022-12-19 13:52:59 -0600

    service ready to use

--- src/app/services/cart-service.service.ts
+++ src/app/services/cart-service.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartServiceService {
+public cartItemList : any = []
+
+public productList = new BehaviorSubject<any>([])
+
 
   constructor() { }
+
+  getProducts(){
+   return this.productList.asObservable()
+  }
+
+  setProduct (product : any){
+    this.cartItemList.push(...product)
+    this.productList.next(product)
+  }
+
+  addToCart (product:any){
+    this.cartItemList.push(product)
+    this.productList.next(this.cartItemList)
+    this.getTotalPrice()
+  }
+
+  getTotalPrice(){
+    let granTotal = 0
+    this.cartItemList.map((a:any)=>{
+      granTotal+= a.total
+    })
+  }
+
+  removeCartItem(product: any){
+    this.cartItemList.map((a:any, index: any)=>{
+      if(product.id === a.id){
+        this.cartItemList.splice(index, 1)
+      }
+    })
+  }
+  removeAllCart(){
+    this.cartItemList = []
+    this.productList.next(this.cartItemList)
+  }
 }
