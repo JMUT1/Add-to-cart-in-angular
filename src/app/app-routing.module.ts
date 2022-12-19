2580209a2556931716eeb5cf5f7803f824244a88
Chema Torres <chemaaa94@gmail.com>
2022-12-19 13:36:50 -0600

    create a new service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductComponent} from "../app/components/product/product.component"
+import {CartComponent} from "../app/components/cart/cart.component"
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: "", redirectTo: "products", pathMatch: "full"},
+  {path: "products", component: ProductComponent},
+  {path: "cart", component: CartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
